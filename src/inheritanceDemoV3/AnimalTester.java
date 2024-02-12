c119c4f4a157f14ef6d44d746d37b5dbd34b978e
aSenghore <asenghore@ggc.edu>
2024-02-12 11:28:27 -0500

    Animal tester inheritance

--- src/inheritanceDemoV3/AnimalTester.java
+++ src/inheritanceDemoV3/AnimalTester.java
@@ -3,10 +3,24 @@ package inheritanceDemoV3;
 public class AnimalTester {
 	public static void main(String[] args) {
 		Mammal mammal = new Mammal();
+		Mammal mammal2 = new Mammal(true, true);
+		Mammal mammal3 = new Mammal(true, 4, "Omnivore", false, true);
 		Reptile reptile = new Reptile();
+		Reptile reptile2 = new Reptile(false,true);
+		Reptile reptile3 = new Reptile(true, 4, "Carnivore", true, true);
+		
 		mammal.makeNoise();
 		mammal.eat();
-		reptile.makeNoise();
+		mammal2.makeNoise();
+		mammal2.eat();
+		mammal3.makeNoise();
+		mammal3.eat();
+		
+		mammal.makeNoise();
 		mammal.eat();
+		mammal2.makeNoise();
+		mammal2.eat();
+		mammal3.makeNoise();
+		mammal3.eat();
 	}
 }
